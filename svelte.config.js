8aaa4fd120383d5a1dc3ce8afca87eedf3cae6da
AngelMApps <angel456mora123@gmail.com>
2022-01-16 20:42:01 -0500

    prueba 2 split true

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,9 @@
 import adapter from '@sveltejs/adapter-netlify';
-
+//import firebase from "svelte-adapter-firebase";
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({split: false}),
+		adapter: adapter({split: true}),
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
